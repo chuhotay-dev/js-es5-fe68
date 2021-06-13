dc71edc896f4661023f9d3cd090c5cf1f20d9ab7
Chu Ho Tay <chuhotay.dev@gmail.com>
2021-06-13 17:06:58 +0700

    es5 buoi 1

---
+++ es5-buo1/main.js
@@ -0,0 +1,180 @@
+// Dùng cho việc debug
+console.log('Hello World!');
+
+// alert('Hello');
+
+/**
+ * Biến và kiểu dữ liệu:
+ * - Kiểu nguyên thuỷ (primitive type): string, number, boolean, undefined, null, bigint, symbol
+ * - null là kiểu object => bug
+ * - Còn lại là object
+ * - Phép gán (=)
+ * - Dynamic type
+ * - Đặt tên biến theo kiểu camelCase (kiểu lạc đà) và phải có ý nghĩa
+ * - Không bắt đầu bằng số và kí tự đặc biệt, không được chứa kí tự đặc biệt ngoại trừ dấu gạch dưới
+ * - Không đặt tên biến có khoảng trắng
+ * - Không đặt tên biến trùng với các keywords của JS: if, else, function, var,...
+ * - Hằng số (constant):
+ *      + Lưu trữ những giá trị không bao giờ thay đổi
+ *      + Không thể gán lại giá trị mới được
+ *      + Bắt buộc phải gán giá trị ngay lúc khởi tạo biến
+ *      + Khai báo hằng số nên viết IN_HOA
+ */
+
+var fullName = 'Chu Ho Tay'; // string
+console.log(typeof fullName);
+
+var age = 18; // number
+console.log(typeof age);
+
+age = '18';
+console.log(typeof age);
+
+var isStudent = true; // boolean
+console.log(typeof isStudent);
+
+var height; // undefined
+console.log(typeof height);
+
+var weight = null; // object => bug
+console.log(typeof weight);
+
+
+// var _a => ok
+// var 2a = ''; => toang
+// var a2 = ''; => ok
+// var ?a = ''; => toang
+// var a-b = ''; => toang
+// var a_b = ''; => ok
+// var if = ''; => toang
+
+// const PI; => toang
+const PI = 3.14;
+// PI = 3.15; => toang
+
+/**
+ * Câu lệnh điều kiện:
+ * - if 
+ * - if else 
+ * - else if
+ * - switch case: chỉ so sánh bằng
+ * Phép so sánh:
+ * - ==: Chỉ so sánh giá trị
+ * - ===: So sánh giá trị và kiểu dữ liệu
+ * - >, <, >=, <=, !=, !==
+ * Biểu thức điều kiện
+ * - AND (&&): tất cả true thì cả biểu thức true, 1 thằng false thì cả biểu thức false
+ *  + Nếu biểu thức trước && mà true => chạy tiếp
+ *  + Nếu biểu thức trước && mà false => dừng và trả lại giá trị tại ví trí đó
+ * - OR (||): chỉ cần 1 thằng true thì cả biểu thức true, tất cả false thì cả biểu thức false
+ *  + Nếu biểu thức trước || mà false => chạy tiếp
+ *  + Nếu biểu thức trưỡc || mà true => dừng và trả lại giá trị tại ví trí đó
+ * - NOT (!): nghịch đảo lại giá trị boolean hiện tại, vd: !tru => false, !false => true
+ * Truthy và Falsy values
+ * - false: 0, '', "", ``, null, undefined, false, NaN (Not a Number)
+ * - true: còn lại
+ */
+
+// Phép so sánh
+console.log(3 == '3'); // true
+console.log(3 === '3') // false
+console.log(3 != 3); // false
+console.log(3 !== '3'); // true
+
+// if
+var isGirl = false;
+
+if (isGirl) {
+    alert('Hey girl!');
+}
+
+// If else
+var isAdult = false;
+
+if (isAdult) {
+    console.log('You can access this page!');
+} else {
+    console.log('FBI warning!!!');
+}
+
+// Else if
+var trafficLight = 'green';
+
+if (trafficLight == 'green') {
+    console.log('Đi đi');
+} else if (trafficLight == 'yellow') {
+    console.log('Chạy chậm lại');
+} else if (trafficLight == 'red') {
+    console.log('Dừng lại');
+} else {
+    console.log('Cúp điện rồi');
+}
+
+// Switch case
+var number = 1;
+
+switch (number) {
+    case 1:
+        console.log('Một');
+        break;
+    case 2:
+        console.log('Hai');
+        break;
+    case 3: 
+        console.log('Ba');
+        break;
+    case 4: 
+        console.log('Bốn');
+        break;
+    case 5: 
+        console.log('Năm');
+        break;
+    default:
+        console.log('Không xác định');                
+}
+
+
+// Biểu thức điều kiện (LOGICAL OPERATOR)
+var A = true;
+var B = false;
+
+console.log(A && B); // false
+console.log(A || B); // true
+console.log(!(!A || B)); // true
+
+
+/**
+ * Hàm (Function)
+ * - 3 loại: declaration func, expression func, arrow func (es6)
+ * - Hàm không tham số, không có giá trị trả về
+ * - Hàm có tham số, không có giá trị trả về
+ * - Hàm vừa có tham số vừa có giá trị trả về
+ * - Tên function phải có ý nghĩa, nên bắt đầu bằng động từ
+ */
+
+// delaration
+// Hàm không tham số, không có giá trị trả về
+function sayHello() {
+    console.log('Hello there!');
+}
+
+// Hàm có tham số, không có giá trị trả về
+function sayHelloToSomeone(name) {
+    console.log('Hello ' + name)
+}
+
+// Hàm vừa có tham số vừa có giá trị trả về
+function calculateGPA(score1, score2, score3) {
+    var gpa = (score1 + score2 + score3) / 3;
+    return gpa;
+}
+
+// Gọi function
+sayHello();
+sayHelloToSomeone('Tay');
+var _gpa = calculateGPA(7, 5, 8);
+console.log(_gpa);
+
+
+
+
